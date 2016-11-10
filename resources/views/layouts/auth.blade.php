de91e835f668f782575f75b2eda6ae5dc201d500
masakisaaan <masakilex@gmail.com>
2016-11-10 21:33:04 +0900

    スタイリング..

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -22,12 +22,6 @@
 <nav class="navbar navbar-inverse">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarEexample7">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
 			<a class="navbar-brand" href="/">
 				酒
 			</a>
