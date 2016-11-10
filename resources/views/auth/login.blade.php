53f8bda1798499a0efcaa75c82f17a4923620498
masakisaaan <masakilex@gmail.com>
2016-11-10 21:22:50 +0900

    ログインページスタイリング

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,9 @@
 <div class="container-fruid">
     
     <div class="omb_login login_margin">
-    	<h1 class="omb_authTitle">ログイン</h3>
+    	<h1 class="omb_authTitle">ログイン</h1>
+        <h2 class="omb_authsocialTitle">ソーシャルアカウントでログイン</h2>
+
 		<div class="row omb_row-sm-offset-3 omb_socialButtons">
     	    <div class="col-xs-4 col-sm-2">
 		        <a href="#" class="btn btn-lg btn-block omb_btn-facebook">
@@ -39,7 +41,7 @@
 		<div class="row omb_row-sm-offset-3 omb_loginOr">
 			<div class="col-xs-12 col-sm-6">
 				<hr class="omb_hrOr">
-				<span class="omb_spanOr">or</span>
+				<span class="omb_spanOr">または</span>
 			</div>
 		</div>
 
@@ -63,11 +65,11 @@
     	</div>
 		<div class="row omb_row-sm-offset-3">
 			<div class="col-xs-12 col-sm-3">
-				<!--
+				
                 <label class="checkbox">
-					<input type="checkbox" value="remember-me">Remember Me
+					<!-- <input type="checkbox" value="remember-me">Remember Me -->
 				</label>
-                -->
+            
 			</div>
 			<div class="col-xs-12 col-sm-3">
 				<p class="omb_forgotPwd">
